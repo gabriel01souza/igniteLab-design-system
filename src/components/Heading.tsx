7861804edf61c1c17c6bb8b4991ad3ee33f7bb0a
Gabriel Cardoso de Souza <gabriel.cardoso@paytrack.com.br>
2022-10-15 20:36:34 -0300

    Criando tela de login com os componentes criados

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,9 +3,10 @@ import  clsx from  'clsx' ;
 export interface HeadingProps {
     size?: 'sm' | 'md' | 'lg';
     children: string;
+    className?: string;
 }
 
-export function Heading({ size = 'md', children }: HeadingProps) {
+export function Heading({ size = 'md', children, className }: HeadingProps) {
     return (
         <span className={clsx( 
                 'text-gray-100 font-bold font-sans',
@@ -13,11 +14,11 @@ export function Heading({ size = 'md', children }: HeadingProps) {
                     'text-lg': size === 'sm',
                     'text-xl': size === 'md',
                     'text-2xl': size === 'lg',
-                }
+                },
+                className
             )}
         >
             {children}
-            Heading test
         </span>
         )
 }
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,9 +3,10 @@ import  clsx from  'clsx' ;
 export interface TextProps {
     size?: 'sm' | 'md' | 'lg';
     children: string;
+    className?: string;
 }
 
-export function Text({ size = 'md', children }: TextProps) {
+export function Text({ size = 'md', children, className }: TextProps) {
     return (
         <span className={clsx( 
                 'text-gray-100 font-sans',
@@ -13,7 +14,8 @@ export function Text({ size = 'md', children }: TextProps) {
                     'text-xs': size === 'sm',
                     'text-sm': size === 'md',
                     'text-md': size === 'lg',
-                }
+                },
+                className
             )}
         >
             {children}
